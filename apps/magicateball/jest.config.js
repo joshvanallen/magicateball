8d83e47b5f2eae46f262fb4295167bd4e6d506eb
Josh Van Allen <josh.vanallen@gmail.com>
2019-05-11 21:52:52 -0400

    Using NRWL project structure Signed-off-by: Josh Van Allen <josh.vanallen@gmail.com>

---
+++ apps/magicateball/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "magicateball",
+  preset: "../../jest.config.js",
+  coverageDirectory: "../../coverage/apps/magicateball/",
+  snapshotSerializers: [
+    "jest-preset-angular/AngularSnapshotSerializer.js",
+    "jest-preset-angular/HTMLCommentSerializer.js"
+  ]
+};
