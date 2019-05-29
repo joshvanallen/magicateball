13bbb5750815a3df8636a2cc3cad1c3df1deae84
Josh Van Allen <josh.vanallen@gmail.com>
2019-05-29 19:59:31 -0400

    Updated project to v8 Signed-off-by: Josh Van Allen <josh.vanallen@gmail.com>

--- apps/magicateball/src/environments/environment.ts
+++ apps/magicateball/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  gcpAPIKey: ''
+    production: false,
+    gcpAPIKey: 'AIzaSyC_PCZYlPVUPeLdZpuRhug2oLSNKwxGbns'
 };
 
 /*
